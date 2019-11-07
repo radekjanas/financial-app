f435f4f1dfd0915fd671f40bce344066606375b3
Radek Janas <radek@adevo.pl>
2019-11-07 23:45:49 +0100

    Updates

---
+++ precache-manifest.aa4ba2cbb258761c543d90406c06ce29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74ead8f65e8358692b7b29eed023d52",
+    "url": "/financial-app/index.html"
+  },
+  {
+    "revision": "a21a2f903e6489d1922c",
+    "url": "/financial-app/static/css/main.13946977.chunk.css"
+  },
+  {
+    "revision": "32cbc34da7f17f524829",
+    "url": "/financial-app/static/js/2.75b93e65.chunk.js"
+  },
+  {
+    "revision": "a21a2f903e6489d1922c",
+    "url": "/financial-app/static/js/main.f851943a.chunk.js"
+  },
+  {
+    "revision": "7b9fb30a042e666888b4",
+    "url": "/financial-app/static/js/runtime-main.67e2fc20.js"
+  },
+  {
+    "revision": "f3db48079f940c36b373b89b9ea575da",
+    "url": "/financial-app/static/media/debut_dark.f3db4807.png"
+  }
+]);
